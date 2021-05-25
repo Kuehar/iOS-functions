3933bb0f64e5b35ea200340ae7292af1824ae8df
kuehar <green95251@gmail.com>
2021-05-25 22:32:35 +0900

    Initial Commit

---
+++ expressjson/expressjsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  expressjsonApp.swift
+//  expressjson
+//
+//  Created by kuehar on 2021/05/25.
+//
+
+import SwiftUI
+
+@main
+struct expressjsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
